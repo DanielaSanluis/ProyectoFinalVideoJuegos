504fade13e8100bb901f2135a743393314306acf
Sofia <anasofiahdzz@ciencias.unam.mx>
2025-05-24 19:01:06 -0600

    error nivel 3 solucionado

--- Assets/scripts/cristalMorado.cs
+++ Assets/scripts/cristalMorado.cs
@@ -2,43 +2,10 @@ using UnityEngine;
 
 public class cristalMorado : MonoBehaviour
 {
-    public SpawnerCompanero spawner; //para que aparezca un compeñaero
-
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //para que aparezca un compañero
-            Debug.Log("Jugador tocó el cristal morado.");
-
-            if (spawner == null)
-            {
-                Debug.LogWarning("¡SpawnerCompanero no está asignado!");
-                return;
-            }
-
-            int index = spawner.companeroIndexSeleccionado;
-            Debug.Log("Índice del compañero: " + index);
-
-            if (index >= 0)
-            {
-                GameObject companero = spawner.personajesEnEscena[index];
-                if (companero != null)
-                {
-                    companero.SetActive(true);
-                    Debug.Log("Compañero activado desde cristal: " + companero.name);
-                }
-                else
-                {
-                    Debug.LogWarning("El objeto compañero es null.");
-                }
-            }
-            else
-            {
-                Debug.LogWarning("Índice de compañero inválido.");
-            }
-
-            //se activan las protecciones (vidas extra)
             GameObject canvas = GameObject.Find("Canvas");
             if (canvas != null)
             {
@@ -52,22 +19,14 @@ public class cristalMorado : MonoBehaviour
                     if (p.vidasExtra != null)
                         p.vidasExtra.SetActive(true);
 
-                    if (p.ContadorVidasExtra  != null)
+                    if (p.ContadorVidasExtra != null)
                     {
                         p.ContadorVidasExtra.text = p.proteccionesRestantes.ToString();
+
                         p.ContadorVidasExtra.gameObject.SetActive(true); //tambien se active el contador de vidas extra 
                     }
                 }
             }
-            //para que aparezca un compañero
-            /*
-            int index = spawner.companeroIndexSeleccionado;
-            if (index >= 0)
-            {
-                spawner.personajesEnEscena[index].SetActive(true);
-                Debug.Log("Compañero activado desde cristal.");
-            }
-            */
 
             Destroy(gameObject); // destruye el cristal morado 
         }
